96482e7caf748749776be49e7c1b201e8dc9cb6b
rtrevinnoc <rtrevinnoc@wearebuildingthefuture.com>
2021-12-15 23:50:09 -0600

    Added further info link

--- bot.rb
+++ bot.rb
@@ -5,19 +5,13 @@ class WhatsAppBot < Sinatra::Base
   use Rack::TwilioWebhookAuthentication, ENV['TWILIO_AUTH_TOKEN'], '/bot'
 
   post '/bot' do
-    body = params["Body"].downcase
+    body = params["Body"]
 
     answer = HTTParty.get('https://wearebuildingthefuture.com/_answer', :query => {"query" => body})
     
     response = Twilio::TwiML::MessagingResponse.new
     response.message do |message|
-      if body.include?("hi")
-        message.body("Hello!")
-      elsif body.include?("goodbye")
-        message.body("Goodbye!")
-      else
-        message.body(answer["result"]["small_summary"])
-      end
+      message.body(answer["result"]["small_summary"] + "\n\nSee more at https://wearebuildingthefuture.com/?q=" + body.gsub!(/\s/, '+'))
     end
 
     content_type "text/xml"
